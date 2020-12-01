8d97de305bddaa81dcb386bde938e392e02cf165
Klaudia Budzynska <klaudia.budzynska1@gmail.com>
2020-12-01 18:20:44 +0100

    Sass structure

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.scss';
-import App from './App';
+import './scss/index.scss';
+import LandingPage from './js/pages/LandingPage';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <LandingPage />
   </React.StrictMode>,
   document.getElementById('root')
 );
